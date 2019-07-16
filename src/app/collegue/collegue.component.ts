300667dc5b8c5d5cb0f6f90255ba4d42db720be7
Jean-Baptiste Tâm MATHIEU <jbtmathieu@gmail.com>
2019-07-16 16:18:23 +0200

    tp #04 refactoring 2

--- src/app/collegue/collegue.component.ts
+++ src/app/collegue/collegue.component.ts
@@ -18,6 +18,11 @@ export class CollegueComponent implements OnInit {
 
     }
   }
+  
+versNouveauCollab(){
+
+}
+
 reset(saisieTexte:HTMLInputElement){
   saisieTexte.value="";
   saisieTexte.focus();
