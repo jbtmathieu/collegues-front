288bf9e85d77246bbed6a87ca6970ba67fed9d07
Jean-Baptiste Tâm MATHIEU <jbtmathieu@gmail.com>
2019-07-24 14:51:53 +0200

    Backend URL updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   
   // ajout d'une URL backend en mode développement
-  backendUrl: 'https://collegues-api-jpa.herokuapp.com/'
+  backendUrl: 'https://jbtmathieu.github.io/collegues-front/'
 
 };
 
